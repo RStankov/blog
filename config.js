0269c6b95c1230e2f449e090b8074b6076fe9815
Radoslav Stankov <rstankov@gmail.com>
2014-12-28 19:08:23 +0200

    Minor config cleanup

--- config.js
+++ config.js
@@ -1,18 +1,16 @@
-// # Ghost Configuration
-// Setup your Ghost install for various environments
+// Ghost Configuration
 // Documentation can be found at http://support.ghost.org/config/
+
 var path = require('path');
 
 module.exports = {
-  // ### Production
-  // When running Ghost in the wild, use the production environment
-  // Configure your URL and mail settings here
   production: {
     url: 'http://rstankov-blog.herokuapp.com/',
-    fileStorage: false,
     mail: {},
+    fileStorage: false,
+
     database: {
-    client: 'postgres',
+      client: 'postgres',
       connection: {
         host: process.env.POSTGRES_HOST,
         user: process.env.POSTGRES_USER,
@@ -22,35 +20,20 @@ module.exports = {
       },
       debug: false
     },
+
     server: {
       host: '0.0.0.0',
       port: process.env.PORT
     },
+
     paths: {
       contentPath: path.join(__dirname, '/content/')
     }
   },
 
-  // ### Development **(default)**
   development: {
-    // The url to use when providing links to the site, E.g. in RSS and email.
-    // Change this to your Ghost blogs published URL.
     url: 'http://localhost:2368',
 
-    // Example mail config
-    // Visit http://support.ghost.org/mail for instructions
-    // ```
-    //  mail: {
-    //      transport: 'SMTP',
-    //      options: {
-    //          service: 'Mailgun',
-    //          auth: {
-    //              user: '', // mailgun username
-    //              pass: ''  // mailgun password
-    //          }
-    //      }
-    //  },
-    // ```
     database: {
       client: 'sqlite3',
       connection: {
@@ -58,12 +41,14 @@ module.exports = {
       },
       debug: false
     },
+
     server: {
       // Host to be passed to node's `net.Server#listen()`
       host: '127.0.0.1',
       // Port to be passed to node's `net.Server#listen()`, for iisnode set this to `process.env.PORT`
       port: '2368'
     },
+
     paths: {
       contentPath: path.join(__dirname, '/content/')
     }
